57e79894bc90e56d2becc2cbbfdc43b72a05ba5f
David Strawn <isomarcte@gmail.com>
2021-06-03 07:28:00 -0600

    Update Release Script
    Update to run with Scala 3 for everything _except_ scalafix and scalafmt.

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ set -e
 
 source sbt-shell-release/src/bash/functions.sh
 
-export SBT_TASKS='++2.13.6;scalafixAll --check;scalafmtSbtCheck;scalafmtCheckAll;+test;+test:doc;docs/mdoc --check'
+export SBT_TASKS='+test;+test:doc;docs/mdoc --check;++2.13.6;scalafixAll --check;scalafmtSbtCheck;scalafmtCheckAll'
 readonly SBT_TASKS
 
 sbt_release
